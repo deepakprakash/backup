676dc9078782ddba801f30800d6b5cdbdb0d2b73
deepakprakash <dp@deepakprakash.com>
2014-09-09 15:34:51 +0200

    Invoke backup only if configured backup disks are connected
    - Previously, backup script was getting triggered for mounting any
    volume.
    - Now smartly checks to make sure one of the configured backup disks
    are connected before initiating backup.
    - Makes sure that backup happens only once after the disks are
    connected.
    - This is done by changing the plist init type to watch on /Volumes
    instead of 'StartOnMount'. Hence the script is now run whenever
    anything changes in /Volumes (not just on disk mount, but also on
    eject).

--- osx/trigger_backup.sh
+++ osx/trigger_backup.sh
@@ -26,53 +26,82 @@ lock_backup () {
     fi
 }
 
-if is_backup_locked; then
-    # Backup is going on. Don't do anything
-    echo "Lockfile found. Aborting."
-    exit 1
-else
-    # Lock the backup so that another backup doesnt run while this is running.
-    lock_backup
+volume_list_previous=/tmp/.backup_vol_list.previous
+volume_list_current=/tmp/.backup_vol_list.current
+volume_list_diff=/tmp/.backup_vol_list.diff
 
-    # Run the primary backup. All the sanity checks are done inside the
-    # `backup` script. So no need to do it here.
-    if backup --primary; then
-        # Successful
+# Generate an empty .previous if it doesnt exist
+if [ ! -f "$volume_list_previous" ];then
+    touch $volume_list_previous
+fi
 
-        # Show notification.
-        terminal-notifier -title "Backup - Primary" -message "Completed primary backup."
+# List current volumes to .current. One on each line. Directories with a trailing '/'
+ls -1F /Volumes/ > $volume_list_current
 
-        secondary_dest=/Volumes/$BACKUP_VOL_SECONDARY/Depot/
+# Output the diff to .diff
+eval "diff --unchanged-line-format= --old-line-format= --new-line-format='%L' $volume_list_previous $volume_list_current > $volume_list_diff"
 
-        # Check if the secondary volume is connected.
-        if [ -d "$secondary_dest" ]; then
-            # Secondary volume is present
+mv $volume_list_previous $volume_list_previous.old
 
-            # Sleep for a few seconds to avoid notification and confirmation dialog
-            # from popping up together. Nothing wrong in it, but simply avoids
-            # too many things popping up to user at the same time.
-            sleep 2
+# Move the new list to previous for use in next invocation
+mv $volume_list_current $volume_list_previous
 
-            # Ask to start secondary backup
-            if confirm_dialog "Start backup to secondary?"; then
-                # User clicked on 'OK'
 
-                if backup --secondary; then
-                    # Successful
+if cat $volume_list_diff | grep -Ex "$BACKUP_VOL_SECONDARY/|$BACKUP_VOL_PRIMARY/"; then
+    # One or both of the $BACKUP_VOL_SECONDARY, $BACKUP_VOL_PRIMARY was found
+    # in the /Volumes diff. Means that one or both of the disks were connected now.
+    # So continue with the backup
 
-                    # Show notification
-                    terminal-notifier -title "Backup - Secondary" -message "Completed secondary backup."
-                else
-                    # Failed
+    if is_backup_locked; then
+        # Backup is going on. Don't do anything
+        echo "Lockfile found. Aborting."
+        exit 1
+    else
+        # Lock the backup so that another backup doesnt run while this is running.
+        lock_backup
 
-                    # Show notification
-                    terminal-notifier -title "Backup - Secondary" -message "Secondary backup failed. Please check logs."
+        # Run the primary backup. All the sanity checks are done inside the
+        # `backup` script. So no need to do it here.
+        if backup --primary --dry-run; then
+            # Successful
+
+            # Show notification.
+            terminal-notifier -title "Backup - Primary" -message "Completed primary backup."
+
+            secondary_dest=/Volumes/$BACKUP_VOL_SECONDARY/Depot/
+
+            # Check if the secondary volume is connected.
+            if [ -d "$secondary_dest" ]; then
+                # Secondary volume is present
+
+                # Sleep for a few seconds to avoid notification and confirmation dialog
+                # from popping up together. Nothing wrong in it, but simply avoids
+                # too many things popping up to user at the same time.
+                sleep 2
+
+                # Ask to start secondary backup
+                if confirm_dialog "Start backup to secondary?"; then
+                    # User clicked on 'OK'
+
+                    if backup --secondary --dry-run; then
+                        # Successful
+
+                        # Show notification
+                        terminal-notifier -title "Backup - Secondary" -message "Completed secondary backup."
+                    else
+                        # Failed
+
+                        # Show notification
+                        terminal-notifier -title "Backup - Secondary" -message "Secondary backup failed. Please check logs."
+                    fi
                 fi
             fi
+
         fi
 
+        # Unlock so that backup can happen again.
+        unlock_backup
     fi
-
-    # Unlock so that backup can happen again.
-    unlock_backup
+else
+    echo "Either a disk was ejected or newly mounted disk(s) are not a PRIMARY or SECONDARY backup volume."
 fi
