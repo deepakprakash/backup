0670a7f85e193ababb2bfce20e65803573b98485
deepakprakash <dp@deepakprakash.com>
2014-09-09 10:02:09 +0200

    Installation script
    Copies the trigger_backup.sh script to /usr/local/bin
    Installs and loads the .plist file into ~/Library/LaunchAgents

---
+++ osx/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if cp -f trigger_backup.sh /usr/local/bin/; then
+    echo "Copied trigger_backup.sh to /usr/local"
+
+    if cp local.backup.trigger.plist ~/Library/LaunchAgents/; then
+        echo "Installed .plist file"
+
+        launchctl unload ~/Library/LaunchAgents/local.backup.trigger.plist
+        launchctl load ~/Library/LaunchAgents/local.backup.trigger.plist
+        echo "Installation complete."
+    fi
+fi
